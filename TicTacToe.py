67c03995e9506567d2521ef6710ed528ecde9713
fuad <fuad@gmail.com>
2021-08-25 02:50:35 +0600

    All required functions were added but still empty!

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+def insertLetter(letter, pos):
+    pass
+
+def spaceIsFree(pos):
+    pass
+
+def printBoard(board):
+    pass
+
+def isWinner(bo, le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove():
+    pass
+
+def selectRandom(board):
+    pass
+
+def isBoardFull(board):
+    pass
+
 def main():
     pass
 
