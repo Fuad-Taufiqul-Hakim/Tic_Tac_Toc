cfe3de1926623301e9f33c61d2accde7288f38bf
fuad <fuad@gmail.com>
2021-08-25 03:28:38 +0600

    Some of the function with main was filled. This is the first half of the Tic Tac Toe. We will finish it later.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python3
 
+board = [" " for x in range(10)]
+
 def insertLetter(letter, pos):
-    pass
+    board[pos] = letter
 
 def spaceIsFree(pos):
-    pass
+    return board[pos] == " "
 
 def printBoard(board):
-    pass
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
 
 def isWinner(bo, le):
-    pass
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or
+    (bo[4] == le and bo[5] == le and bo[6] == le) or
+    (bo[1] == le and bo[2] == le and bo[3] == le) or
+    (bo[1] == le and bo[4] == le and bo[7] == le) or
+    (bo[2] == le and bo[5] == le and bo[8] == le) or
+    (bo[3] == le and bo[6] == le and bo[9] == le) or
+    (bo[1] == le and bo[5] == le and bo[9] == le) or
+    (bo[3] == le and bo[5] == le and bo[7] == le)
 
 def playerMove():
-    pass
+    run = True
+    while run:
+        move = input("Please select a position to place an 'x' (1-9): ")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('x', move)
+                else:
+                    print("Sorry, this space is occupied!")
+            else:
+                print('Please type number within the range!')
+        except:
+            print("Please type a number!")
 
 def compMove():
     pass
@@ -22,9 +55,31 @@ def selectRandom(board):
     pass
 
 def isBoardFull(board):
-    pass
+    if board.count(' ') > 1:
+        return True
+    else:
+        return False
 
 def main():
-    pass
+    print("Welcome to the Tic Tac Toe world!")
+    printBoard()
+
+    while not isBoardFull(board):
+        if not (isWinner(board, 'o')):
+            playerMove()
+            printBoard()
+        else:
+            print("Sorry! o\'s won this time!")
+            break
+
+        if not (isWinner(board, 'x')):
+            compMove()
+            printBoard()
+        else:
+            print("x\'s won this time! Good Job!")
+            break
+
+    if isBoardFull(board):
+        print("Tie game!")
 
 main()
